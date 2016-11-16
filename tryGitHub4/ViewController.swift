48d6565c87478ea774393c78ea478550c6d14a66
Brendan Miriam <hurlbutfree@outlook.com>
2016-11-16 06:30:45 -1000

    Initial Commit

---
+++ tryGitHub4/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  tryGitHub4
+//
+//  Created by mac on 11/16/16.
+//  Copyright © 2016 net.zvezo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("running attempt 4")
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
